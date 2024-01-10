a5147c5cbdf12fcdb1c11efd0a5169dcb46f95d7
Antoan Furlanski <antoan.furlanski@gmail.com>
2024-01-10 13:07:06 +0200

    Update CRUDexc.sql

--- CRUDexc/CRUDexc.sql
+++ CRUDexc/CRUDexc.sql
@@ -4,15 +4,15 @@
 	SELECT * FROM Departments
 
 --02.Find all Department Names
---Create a SQL query that finds all Department name
+--Create a SQL query that finds all Departments names
 	SELECT [Name] FROM Departments
 
 --03.Find Salary of Each Employee
---Create a SQL query that finds the first name, last name and salary for each employe
+--Create a SQL query that finds the first name, last name and salary for each employee
 	SELECT FirstName, LastName, Salary FROM Employees
 
 --04.Find Full Name of Each Employee
---Create a SQL query that finds the first, middle and last name for each employe
+--Create a SQL query that finds the first, middle and last name for each employee
 	SELECT FirstName, MiddleName, LastName FROM Employees
 
 --05.Find Email Address of Each Employee
@@ -22,7 +22,7 @@
 	SELECT	CONCAT_WS('.', FirstName, LastName) + '@softuni.bg' AS [Full Email Adress] 
 	FROM Employees
 
---06.Find All Different Employees' Salaries
+--06.Find All Different Employee's Salaries
 --Create a SQL query that finds all different salaries of the employees. Display the salaries only in one column, named "Salary"
 	SELECT DISTINCT Salary FROM Employees AS [Salary]
 
@@ -71,10 +71,10 @@
 
 --14.Sort Employees Table
 --Create a SQL query that sorts all the records in the Employees table by the following criteria:
---· By salary in decreasing order
---· Then by the first name alphabetically
---· Then by the last name descending
---· Then by middle name alphabetically
+--Â· By salary in decreasing order
+--Â· Then by the first name alphabetically
+--Â· Then by the last name descending
+--Â· Then by middle name alphabetically
 	SELECT * FROM Employees
 	ORDER BY Salary DESC, FirstName ASC, LastName DESC, MiddleName ASC
 
